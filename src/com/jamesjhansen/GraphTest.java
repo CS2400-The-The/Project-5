e96bb7d664ead008176508c304f43909bd577088
jamesjhansen <jamesjhansenh@gmail.com>
2022-04-27 20:28:02 -0700

    Add JUnit skeleton

---
+++ src/com/jamesjhansen/GraphTest.java
@@ -0,0 +1,26 @@
+package com.jamesjhansen;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * contains unit tests for methods in Graph class
+ */
+public class GraphTest {
+
+    /**
+     * handles any setup required prior to testing
+     * @throws Exception
+     */
+    @Before
+    protected void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGraph() {
+        
+    }
+
+}
